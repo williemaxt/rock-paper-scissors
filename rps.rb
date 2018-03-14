cb5fed6f3e3946048fabf061a8b5543efbb610cf
William Thompson <williemaxt@gmail.com>
2018-03-14 11:57:27 -0400

    Add files via upload

---
+++ rps.rb
@@ -0,0 +1,49 @@
+#this prompt teaches a person how to play the game
+puts "Enter Your Number to Play"
+puts "Rock(0) , Paper(1), Scissors(2)"
+
+#establishes a variable that is a random # between 0 and 2. The code below prints it
+number = rand(0..2).to_i
+puts number
+#this assigns a variable to a persons input and displays it
+choice = gets.to_i
+
+ro = 0
+pa = 1
+sc = 2
+
+
+
+
+puts "you chose: "
+puts choice
+
+#this function will be the if statements that controll the game
+def make_decision(number, choice, ro, pa, sc)
+
+  if choice == number 
+    puts "you tied"
+
+  elsif choice == ro && number == sc
+    puts "Player Wins"
+
+  elsif choice == ro && number == pa
+    puts "Computer Wins"
+
+  elsif choice == pa && number == ro
+    puts "Player Wins"
+
+  elsif choice == pa && number == sc
+    puts "Computer Wins"
+
+  elsif choice == sc && number == ro
+    puts "Computer Wins"
+
+  elsif choice == sc && number == pa
+    puts "Player Wins"
+
+  end
+
+end
+
+make_decision(number, choice, ro, pa, sc)
